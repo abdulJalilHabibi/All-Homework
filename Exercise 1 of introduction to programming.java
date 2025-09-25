6133df3064cd46d7462ed87ac9a9d197c5b65427
abdulJalilHabibi <abduljalilhabibi230@gmail.com>
2025-09-25 17:22:40 +0430

    Create Exercise 1 of introduction to programming.java

---
+++ Exercise 1 of introduction to programming.java
@@ -0,0 +1,28 @@
+
+package javaapplication64;
+
+
+public class JavaApplication64 {
+
+ 
+    public static void main(String[] args) {
+        
+        System.out.println("       j         A      V           V        A             ");
+        System.out.println("       j        A A      V         V        A A               ");
+        System.out.println("       j      A     A     V       V        A    A            ");
+        System.out.println("  j    j     AAAAAAAAA     V     V        AAAAAAAA                   ") ;
+        System.out.println("  j    j    A         A     V   V        A        A             ");
+        System.out.println("  j j j    A           A      V         A          A        ");             
+        
+      
+     
+     }
+     
+     
+     
+     
+     
+     
+    }
+    
+
